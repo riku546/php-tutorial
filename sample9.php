ce304fc265e4a292420ea955c6be6c4f58ddf2a1
riku <riku62560@gmail.com>
2024-06-16 15:41:04 +0900

    phpのifを使った

---
+++ sample9.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form action="sample9.php" method="post">
+  <input type="text" name="gender">
+  <input type="submit" value="SEND">
+</form>
+</body>
+</html>
+
+<?php
+$gender = $_POST["gender"];
+if ($gender === "man") {
+    echo "man";
+} elseif ($gender === "woman") {
+    echo "woman";
+} else {
+    echo "other";
+}
