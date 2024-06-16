e5b3fe112cf55e131c276e2e11d6aed832410ca3
riku <riku62560@gmail.com>
2024-06-16 12:24:56 +0900

    計算機出来た

---
+++ sample8.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="sample8.php" method="post">
+    <input type="number" name="num1">
+    <input type="number" name="num2">
+    <input type="submit" value="send">
+  </form>
+</body>
+</html>
+
+
+<?php
+
+if (!$_POST["num1"] || !$_POST["num2"]) {
+    echo "null";
+} else {
+    echo $_POST["num1"] + $_POST["num2"];
+}
