47071615c1ea5a1cf0a82feda601e8c47598646a
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-18 12:20:16 +0900

    ｐｈｐ学習

---
+++ sample12.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+
+
+
+</style>
+
+<body>
+    <form class="form" action="sample12.php" method="post">
+        <input type="text" name="text"  class="input">
+        <input type="number" name="age"  class="input">
+        <input type="submit" value="send" class="button">
+    </form>
+</body>
+
+</html>
+
+<?php
+function foo($arg)
+{
+    if ($arg === "enoki") {
+        echo "hello\n", $arg;
+
+    } else {
+        echo "";
+    }
+
+}
+$arg = $_POST["text"];
+foo($arg);
---
+++ sample13.php
@@ -0,0 +1,7 @@
+<?php
+$book [0][0] = "desgin";
+$book [0][1] = "kiso";
+$book[1][0] = "php";
+echo $book[0][0];
+
+print_r( $book );
