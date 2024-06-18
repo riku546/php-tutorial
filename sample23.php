bb6ba1b1e1d9ac8c18dcfeeb5fda3c0442a66795
riku <riku62560@gmail.com>
2024-06-18 22:06:58 +0900

    無名関数は引数として、別の関数に渡せる

---
+++ sample23.php
@@ -0,0 +1,18 @@
+<?php
+// function sum(int $a, int $b): int
+// {
+//     return $a + $b;
+// }
+
+function echo_price($callback){
+    echo number_format($callback(1000, 2));
+}
+
+//無名関数
+// $total = function (int $x, int $y): int {
+//     return $x + $y;
+// };
+
+$total = fn(int $a , int $b):int => $a + $b;
+
+echo echo_price($total);
