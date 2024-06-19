859ae716c4d6f085db852e9abcff2ce9417c6643
riku <riku62560@gmail.com>
2024-06-19 22:12:15 +0900

    getter setter

--- sample29.php
+++ sample29.php
@@ -2,13 +2,24 @@
 class Item
 {
 
+    public string $name;
+    private int $price;
+
     public function __construct(string $name, int $price = 0)
     {
         $this->name = $name;
         $this->price = $price;
     }
 
-    public function getPrice($end = ""): string
+    public function setPrice(int $price)
+    {
+        if ($price < 0) {
+            return false;
+        }
+        $this->price = $price;
+    }
+
+    public function getPrice(string $end = ""): string
     {
         return number_format($this->price) . $end;
     }
@@ -16,8 +27,7 @@ class Item
 }
 
 $php_basic = new Item("php 入門");
+$php_basic->setPrice(1222);
 
 echo $php_basic->name . "<br>";
 echo $php_basic->getPrice("円");
-
-
