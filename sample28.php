753c868bf79f62b18e8fd6ff116ec82c72be6c0b
riku <riku62560@gmail.com>
2024-06-19 21:57:59 +0900

    コンストラクト

---
+++ sample28.php
@@ -0,0 +1,21 @@
+<?php
+class Item
+{
+
+    public function __construct(string $name, int $price = 0)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getPrice($end = ""): string
+    {
+        return number_format($this->price) . $end;
+    }
+
+}
+
+$php_basic = new Item("php 入門");
+
+echo $php_basic->name . "<br>";
+echo $php_basic->getPrice("円");
