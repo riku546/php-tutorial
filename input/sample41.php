76ffab2a0ba2bc61040de18a0857212fc0c1c28c
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-21 14:09:14 +0900

    作成したインスタンスをクローンできる

---
+++ input/sample41.php
@@ -0,0 +1,13 @@
+<?php
+class Sample{
+    public string $name;
+
+}
+
+
+$sample1 = new Sample();
+$sample1->name = "php";
+
+$sample2 = clone($sample1);
+$sample1-> name = "js";
+echo $sample2->name;
