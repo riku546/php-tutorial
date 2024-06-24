ddefecac15396d510b93b09e384b539669f079c4
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-24 14:33:50 +0900

    掲示板完成

--- bulletin_board/index.php
+++ bulletin_board/index.php
@@ -6,9 +6,42 @@
   <title>Document</title>
 </head>
 <body>
+  <?php
+  require_once 'db.php';
+
+  try {
+    $dbh = new PDO($dsn, $user, $password);
+
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+      $input_name = filter_input(INPUT_POST , "name");
+      $input_text = filter_input(INPUT_POST , "post");
+  
+  
+      $sql = "INSERT INTO posting_info(  name , post)
+       VALUES(:name , :post)";
+  
+       $stmt = $dbh -> prepare($sql);
+       $stmt->bindParam(':name' , $input_name ,PDO::PARAM_STR );
+       $stmt->bindParam(':post' , $input_text ,PDO::PARAM_STR);
+  
+       $stmt->execute();
+  
+      header("Location:http://localhost/php-tutorial/bulletin_board/post_list.php");
+    }
+    
+  } catch (PDOException $e) {
+    echo ('Error:' . $e->getMessage());
+    die();
+  }
+
+
+
+
+
+  ?>
   
   <h1>投稿</h1>
-  <form action="post_list.php" method="post" style="display: flex; flex-direction:column;">
+  <form action=""  method="post" style="display: flex; flex-direction:column;">
     <label for="">
       name: <input type="text" name="name">
     </label>
