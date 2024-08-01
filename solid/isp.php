053d499effde271efac7be4438886d57c8cb5d4d
riku546 <opa9788@gmail.com>
2024-08-01 13:33:39 +0900

    isp

---
+++ solid/isp.php
@@ -0,0 +1,29 @@
+<?php
+
+interface Printer{
+    public function print();
+}
+
+interface Scan{
+  public function scan();
+}
+
+interface fax{
+  public function fax();
+}
+
+interface copyDocument{
+  public function copyDocument();
+}
+
+
+interface emailDocument{
+  public function emailDocument();
+}
+
+interface bindDocument{
+  public function bindDocument();
+}
+
+
+
